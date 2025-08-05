ce7a54ea799c58bd425f148b69ce6472ad36d56b
oshan shanuka <oshanshanuka428@gmail.com>
2025-08-05 20:21:23 +0530

    day 08

---
+++ day8.py
@@ -0,0 +1,12 @@
+import pdb
+
+def multiplication(a, b):
+    return a * b
+
+# pdb.set_trace()
+breakpoint()
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+mul = multiplication(x, y)
+print(mul)
